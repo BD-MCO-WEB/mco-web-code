853749c0a61ea5e01b5219d7a33c419c1014feb6
pytonic <pytonic@foxmail.com>
2013-05-05 21:23:55 +0800

    oioiooio

--- laruence.com/deploy_yac.home.sh
+++ laruence.com/deploy_yac.home.sh
@@ -5,20 +5,19 @@
 cd $(pwd)/yac/
 
 make clean ;
-./configure --enable-yac --with-php-config=/home/work/php/bin/php-config 
+/usr/local/Cellar/php5.4/bin/phpize
+./configure --enable-yac --with-php-config=/usr/local/Cellar/php5.4/bin/php-config
 make 
 make install;
 
-mv /home/work/php/lib/php/extensions/no-debug-non-zts-20090626/yac.so /home/work/php/lib;
-
 re='^[^;].*yac\.so';
-yac_config=`grep $re /home/work/php/lib/php.ini`
+yac_config=`grep $re /usr/local/Cellar/php5.4/lib/php.ini`
 echo $yac_config;
 
 if [ -z "$yac_config" ]; then
     echo "need to enable yac.so in php.ini...\n";
-    echo -e "[yac] \nextension = yac.so" >> /home/work/php/lib/php.ini
+    echo "[yac] \nextension = yac.so" >> /usr/local/Cellar/php5.4/lib/php.ini
 else
-    echo -e "already enabled yac.so in php.ini...\r\nDone";
+    echo "already enabled yac.so in php.ini...\r\nDone";
 fi
 
